6231647f384610cf580c81ab32314a0d39545905
Hoang Doan <dthoang92@gmail.com>
2015-11-11 16:27:47 +0700

    Update README. Update source withdraw_normal: custom url_test variable

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # php-race-condition-example
 Race condition vulnerability example source code.
+
+Or leave it empty and use the [link_blogspot].
+[link_blogspot]: http://blog.hoangdoan.io/2015/11/php-race-condition-vulnerability-example.html
+
--- withdraw_normal.py
+++ withdraw_normal.py
@@ -1,5 +1,8 @@
 import os
 # Withdraw 128 times sequentially, $100 each time.
+
+url_test = 'http://domain1.sec/race/poc.php?user_code=BANK000001&amount=100'
+
 for i in xrange(0,128):
 	print os.popen('php -r ' + \
-                '"echo file_get_contents(\'http://domain1.sec/race/poc.php?user_code=BANK000001&amount=100\');"').read()
+                '"echo file_get_contents(\'' + url_test + '\');"').read()
